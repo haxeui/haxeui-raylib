404bf5c3ebcd18afd4d6003284965e3e2013a5e3
Ian Harrigan <ianharrigan@hotmail.com>
2021-11-29 09:28:30 +0100

    Fix for https://github.com/haxeui/haxeui-raylib/issues/5

--- haxe/ui/backend/ImageDisplayImpl.hx
+++ haxe/ui/backend/ImageDisplayImpl.hx
@@ -3,6 +3,8 @@ package haxe.ui.backend;
 import RayLib.*;
 import RayLib.Texture2D;
 import RayLib.Colors.*;
+import RayLib.Rectangle;
+import RayLib.Vector2;
 
 class ImageDisplayImpl extends ImageBase {
     private var _texture:Texture2D;
@@ -30,7 +32,10 @@ class ImageDisplayImpl extends ImageBase {
         var iy = y + _top;
 
         if (_imageWidth != _imageInfo.width || _imageHeight != _imageInfo.height) {
-            DrawTexture(_texture, Std.int(ix), Std.int(iy), WHITE);
+            var source = Rectangle.create(0, 0, _imageInfo.width, _imageInfo.height);
+            var dest = Rectangle.create(Std.int(ix), Std.int(iy), _imageWidth, _imageHeight);
+            var origin = Vector2.create(0, 0);
+            DrawTexturePro(_texture, source, dest, origin, 0, WHITE);
         } else {
             DrawTexture(_texture, Std.int(ix), Std.int(iy), WHITE);
         }
